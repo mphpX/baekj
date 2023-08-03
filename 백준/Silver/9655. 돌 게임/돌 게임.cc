e2986b8d9ef1376bc25b939a37baf004a3aec22c
jibangdaegongdae <138234182+jibangdaegongdae@users.noreply.github.com>
2023-08-03 23:31:46 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+int main(){
+	int n;
+	scanf("%d",&n);
+	if(n%2==0){
+		printf("CY\n");
+	}
+	else{
+		printf("SK\n");
+	}
+	return 0;
+}
