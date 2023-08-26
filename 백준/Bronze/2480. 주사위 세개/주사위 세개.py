fd4e5d6dcc88c433e6686662f69ecca63c929dd6
jibangdaegongdae <138234182+jibangdaegongdae@users.noreply.github.com>
2023-08-26 17:12:19 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,9 @@
+arr=list(map(int,input().split()))
+arr.sort()
+sum=0
+if(arr[0]==arr[1]==arr[2]):sum=10000+arr[0]*1000
+else:
+    if(arr[0]==arr[1]):sum=1000+arr[0]*100
+    elif(arr[1]==arr[2]):sum=1000+arr[1]*100
+    else:sum=arr[2]*100
+print(sum)
