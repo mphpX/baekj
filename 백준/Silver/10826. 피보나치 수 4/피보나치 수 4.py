f09f7f24566030dd8a3a67d2eef55aa628e69003
jibangdaegongdae <138234182+jibangdaegongdae@users.noreply.github.com>
2023-08-31 23:00:10 +0900

    [Silver V] Title: 피보나치 수 4, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
@@ -1,7 +1,12 @@
 import sys
 n=int(input())
-fibo=[0,1]
-
-for i in range(2,n+1):
-    fibo.append(fibo[i-1]+fibo[i-2])
-print(fibo[n])
+a=0
+b=1
+c=0
+for i in range(1,n):
+    c=a+b
+    a=b
+    b=c
+if(n==0):print(a)
+elif(n==1):print(b)
+else:print(c)
