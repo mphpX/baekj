a87917b319480a1d74883dd103521e2a56222917
mphpX <138234182+mphpX@users.noreply.github.com>
2024-02-25 16:56:47 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 52 ms, Memory: 32140 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -0,0 +1,18 @@
+#a 98
+import sys
+l=input()
+x=[[0 for j in range(26)]for i in range(len(l))]
+x[0][ord(l[0])-97]=1
+n=int(input())
+for i in range(1,len(l)):
+    for j in range(26):
+        x[i][j]=x[i-1][j]
+    x[i][ord(l[i])-97]=x[i-1][ord(l[i])-97]+1
+for i in range(n):
+    a,m,o=sys.stdin.readline().split()
+    m=int(m)
+    o=int(o)
+    if(m>0):
+        print(x[o][ord(a)-97]-x[m-1][ord(a)-97])
+    else:
+        print(x[o][ord(a)-97])
