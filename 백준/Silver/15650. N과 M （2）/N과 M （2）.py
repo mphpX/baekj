c19236ab2d5feef5e1944416dd107dbe9619e79a
mphpX <138234182+mphpX@users.noreply.github.com>
2024-03-05 21:12:22 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,19 +1,13 @@
 import sys
 n,m=map(int,sys.stdin.readline().split())
 s=[]
-def dfs():
+def dfs(start):
     if len(s)==m:
         print(' '.join(map(str,s)))
         return
-    for i in range(1,n+1):
+    for i in range(start,n+1):
         if i not in s:
-            if(len(s)==0):
-                s.append(i)
-                dfs()
-                s.pop()
-            else:
-                if(max(s)<i):
-                    s.append(i)
-                    dfs()
-                    s.pop()
-dfs()
+            s.append(i)
+            dfs(i+1)
+            s.pop()
+dfs(1)
