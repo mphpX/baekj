58b1c84a4c57e933eb0b6feabdd1a294f5db4955
mphpX <138234182+mphpX@users.noreply.github.com>
2024-03-05 21:14:47 +0900

    [Silver III] Title: N과 M (4), Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,12 @@
+import sys
+n,m=map(int,sys.stdin.readline().split())
+s=[]
+def dfs(start):
+    if len(s)==m:
+        print(' '.join(map(str,s)))
+        return
+    for i in range(start,n+1):
+        s.append(i)
+        dfs(i)
+        s.pop()
+dfs(1)
