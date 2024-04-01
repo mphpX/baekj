3c91c34d571b549b115df5d64bbd3fde3089f9d4
mphpX <138234182+mphpX@users.noreply.github.com>
2024-04-01 17:09:31 +0900

    [Silver II] Title: 최대 힙, Time: 128 ms, Memory: 37044 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,14 @@
+import sys
+import heapq
+heap=[]
+n=int(sys.stdin.readline())
+for i in range(n):
+    a=int(sys.stdin.readline())
+    if(a!=0):
+        heapq.heappush(heap,-a)
+    else:
+        if(len(heap)==0):
+            print(0)
+        else:
+            x=heapq.heappop(heap)
+            print(-x)
