4d15421979031a45c0bbcd2b92d1e555bdd70446
mphpX <138234182+mphpX@users.noreply.github.com>
2025-03-15 14:10:10 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,6 +1,5 @@
-import sys
-arr=[0,1,2]
 n=int(input())
+dp=[0,1,2]
 for i in range(3,n+1):
-    arr.append((arr[i-1]+arr[i-2])%10007)
-print(arr[n])
+    dp.append((dp[i-2]+dp[i-1])%10007)
+print(dp[n])
