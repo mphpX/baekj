2b1adf1725a92a87712cdda78e4b767a38523deb
mphpX <138234182+mphpX@users.noreply.github.com>
2025-03-19 15:41:17 +0900

    [Silver I] Title: 포도주 시식, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,15 +1,22 @@
-n=int(input())
-wine=[]
-for i in range(n):
-    a=int(input())
-    wine.append(a)
-dp=[0 for i in range(n)]
-dp[0]=wine[0]
-if(n>1):
-    dp[1]=wine[0]+wine[1]
-    if(n>2):
-        dp[2]=max(dp[1],wine[1]+wine[2])
+import sys
+input = sys.stdin.readline
 
-for i in range(3,n):
-    dp[i]=max(dp[i-2]+wine[i],dp[i-2]-wine[i-2]+wine[i-1]+wine[i],dp[i-3]+wine[i-1]+wine[i])
-print(max(dp))
+n = int(input())
+wine = [int(input()) for _ in range(n)]  # 리스트 컴프리헨션 사용 (더 깔끔하고 빠름)
+
+if n == 1:
+    print(wine[0])
+    exit()
+
+dp = [0] * n
+dp[0] = wine[0]
+
+if n > 1:
+    dp[1] = wine[0] + wine[1]
+if n > 2:
+    dp[2] = max(wine[0] + wine[2], wine[1] + wine[2], dp[1])
+
+for i in range(3, n):
+    dp[i] = max(dp[i-1], dp[i-2] + wine[i], dp[i-3] + wine[i-1] + wine[i])
+
+print(dp[n-1])  # 최대로 마실 수 있는 와인양 출력
