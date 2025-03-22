f774132f78f4a536b16d4db5fa009d21e921289e
mphpX <138234182+mphpX@users.noreply.github.com>
2025-03-22 21:46:44 +0900

    [Gold V] Title: 적록색약, Time: 68 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -5,14 +5,11 @@ input = sys.stdin.readline
 n = int(input())
 gh = [list(input().strip()) for _ in range(n)]
 
-def jbfs(start_x, start_y, graph, is_jr, visited):
+def jbfs(start_x, start_y, graph, wanted, visited):
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
-    queue = deque()
-    queue.append((start_x, start_y))
+    queue = deque([(start_x, start_y)])
     visited[start_x][start_y] = True
-
-    start_color = graph[start_x][start_y]
 
     while queue:
         x, y = queue.popleft()
@@ -20,17 +17,9 @@ def jbfs(start_x, start_y, graph, is_jr, visited):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
-                neighbor_color = graph[nx][ny]
-                if is_jr:
-                    # 적록색약 모드
-                    if (start_color in 'RG' and neighbor_color in 'RG') or start_color == neighbor_color:
-                        visited[nx][ny] = True
-                        queue.append((nx, ny))
-                else:
-                    # 일반 모드
-                    if start_color == neighbor_color:
-                        visited[nx][ny] = True
-                        queue.append((nx, ny))
+                if graph[nx][ny] in wanted:
+                    visited[nx][ny] = True
+                    queue.append((nx, ny))
 
 # 일반 시야
 visited = [[False] * n for _ in range(n)]
@@ -38,7 +27,7 @@ fct = 0
 for i in range(n):
     for j in range(n):
         if not visited[i][j]:
-            jbfs(i, j, gh, False, visited)
+            jbfs(i, j, gh, gh[i][j], visited)
             fct += 1
 
 # 적록색약 시야
@@ -47,7 +36,10 @@ sct = 0
 for i in range(n):
     for j in range(n):
         if not visited[i][j]:
-            jbfs(i, j, gh, True, visited)
+            if gh[i][j] in 'RG':
+                jbfs(i, j, gh, 'RG', visited)
+            else:
+                jbfs(i, j, gh, 'B', visited)
             sct += 1
 
 print(fct, sct)
