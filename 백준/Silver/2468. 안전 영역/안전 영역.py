087e1da1a18e193365db18412201c5d33f0e9cd5
mphpX <138234182+mphpX@users.noreply.github.com>
2025-04-01 15:34:49 +0900

    [Silver I] Title: 안전 영역, Time: 720 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,36 +1,48 @@
 import sys
 from collections import deque
-input=sys.stdin.readline
-n=int(input())
-graph=[]
-for i in range(n):
-    l=list(map(int,input().split()))
-    graph.append(l)
-def bfs(graph,start_x,start_y,limit):
-    q=deque([(start_x,start_y)])
-    dx=[1,-1,0,0]
-    dy=[0,0,1,-1]
-    while(q):
-        cur_x,cur_y=q.popleft()
-        for i in range(4):
-            next_x=cur_x+dx[i]
-            next_y=cur_y+dy[i]
-            if(0<=next_x<n and 0<=next_y<n and visit[next_x][next_y]==False and graph[next_x][next_y]>limit):
-                visit[next_x][next_y]=True
-                q.append((next_x,next_y))
 
-ans=[]
-h=0
-while(True):
-    ct=0
-    visit=[[False for _ in range(n)]for _ in range(n)]
+input = sys.stdin.readline
+
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+# BFS로 안전 영역 탐색
+def bfs(start_x, start_y, limit):
+    q = deque([(start_x, start_y)])
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+
+    while q:
+        cur_x, cur_y = q.popleft()
+        for i in range(4):
+            next_x = cur_x + dx[i]
+            next_y = cur_y + dy[i]
+            if (
+                0 <= next_x < n and 0 <= next_y < n
+                and not visit[next_x][next_y]
+                and graph[next_x][next_y] > limit
+            ):
+                visit[next_x][next_y] = True
+                q.append((next_x, next_y))
+
+# 높이(h)를 0부터 1씩 올려가며 시뮬레이션
+ans = []
+h = 0
+
+while True:
+    ct = 0
+    visit = [[False] * n for _ in range(n)]
+
     for i in range(n):
         for j in range(n):
-            if(graph[i][j]>h and visit[i][j]==False):
-                bfs(graph,i,j,h)
-                ct+=1
+            if graph[i][j] > h and not visit[i][j]:
+                bfs(i, j, h)
+                ct += 1
+
     ans.append(ct)
-    h+=1
-    if(ct==0):
+    h += 1
+    if ct == 0:
         break
+
+# 안전 영역 최대 개수 출력
 print(max(ans))
