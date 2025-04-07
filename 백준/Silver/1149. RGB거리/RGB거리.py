77f4b79cd73c233d2d155bab210fb9cb05c2279d
mphpX <138234182+mphpX@users.noreply.github.com>
2025-04-07 14:44:56 +0900

    [Silver I] Title: RGB거리, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,11 +1,11 @@
 import sys
 input=sys.stdin.readline
 n=int(input())
-graph=[list(map(int,input().split())) for _ in range(n)]
-cur_r,cur_g,cur_b=graph[0][0],graph[0][1],graph[0][2]
+cur_r,cur_g,cur_b = map(int,input().split())
 for i in range(1,n):
-    next_r=min(cur_g,cur_b)+graph[i][0]
-    next_g=min(cur_r,cur_b)+graph[i][1]
-    next_b=min(cur_r,cur_g)+graph[i][2]
+    r,g,b=map(int,input().split())
+    next_r=min(cur_g,cur_b)+r
+    next_g=min(cur_r,cur_b)+g
+    next_b=min(cur_r,cur_g)+b
     cur_r,cur_g,cur_b=next_r,next_g,next_b
 print(min(next_r,next_g,next_b))
