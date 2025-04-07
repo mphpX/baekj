3c9835733af6ad938932c03804d8be1dca99b87e
mphpX <138234182+mphpX@users.noreply.github.com>
2025-04-07 13:53:54 +0900

    [Silver I] Title: RGB거리, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,18 +1,11 @@
 import sys
-arr=[]
+input=sys.stdin.readline
 n=int(input())
-for i in range(n):
-    num=list(map(int,sys.stdin.readline().split()))
-    arr.append(num)
-dp=[[arr[0][0],arr[0][1],arr[0][2]]]
+graph=[list(map(int,input().split())) for _ in range(n)]
+dp=[[0,0,0] for _ in range(n)]
+dp[0][0],dp[0][1],dp[0][2]=graph[0][0],graph[0][1],graph[0][2]
 for i in range(1,n):
-    num=[]
-    for j in range(3):
-        if(j==0):
-            num.append(min(dp[i-1][1],dp[i-1][2])+arr[i][j])
-        elif(j==1):
-            num.append(min(dp[i-1][0],dp[i-1][2])+arr[i][j])
-        else:
-            num.append(min(dp[i-1][1],dp[i-1][0])+arr[i][j])
-    dp.append(num)
+    dp[i][0]=min(dp[i-1][1],dp[i-1][2])+graph[i][0]
+    dp[i][1]=min(dp[i-1][0],dp[i-1][2])+graph[i][1]
+    dp[i][2]=min(dp[i-1][0],dp[i-1][1])+graph[i][2]
 print(min(dp[n-1]))
