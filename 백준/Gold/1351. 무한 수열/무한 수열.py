f7717f4fbb66718d928de0686dfbe681c78f3948
mphpX <138234182+mphpX@users.noreply.github.com>
2025-04-10 19:31:44 +0900

    [Gold V] Title: 무한 수열, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1351. 무한 수열/무한 수열.py
+++ 백준/Gold/1351. 무한 수열/무한 수열.py
@@ -1,14 +1,9 @@
-from collections import deque
 n,p,q=map(int,input().split())
 d=dict()
 d[0]=1
 def solve(n,a,b):
     if(n in d):
         return d[n]
-    else:
-        if(n//a not in d):
-            d[n//a]=solve(n//a,a,b)
-        if(n//b not in d):
-            d[n//b]=solve(n//b,a,b)
-        return d[n//a]+d[n//b]
+    d[n]=solve(n//a,a,b)+solve(n//b,a,b)
+    return d[n]
 print(solve(n,p,q))
