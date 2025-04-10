9acbdd8ff95263736a0a802e6cf8dc615522edea
mphpX <138234182+mphpX@users.noreply.github.com>
2025-04-10 19:23:38 +0900

    [Gold V] Title: 무한 수열, Time: 60 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Gold/1351. 무한 수열/무한 수열.py
+++ 백준/Gold/1351. 무한 수열/무한 수열.py
@@ -1,16 +1,14 @@
 from collections import deque
-import sys
-sys.setrecursionlimit(10 ** 6)
 n,p,q=map(int,input().split())
 d=dict()
 d[0]=1
-def dfs(n,a,b):
+def solve(n,a,b):
     if(n in d):
         return d[n]
     else:
         if(n//a not in d):
-            d[n//a]=dfs(n//a,a,b)
+            d[n//a]=solve(n//a,a,b)
         if(n//b not in d):
-            d[n//b]=dfs(n//b,a,b)
+            d[n//b]=solve(n//b,a,b)
         return d[n//a]+d[n//b]
-print(dfs(n,p,q))
+print(solve(n,p,q))
