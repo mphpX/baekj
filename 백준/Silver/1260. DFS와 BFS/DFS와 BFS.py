23212a735babe5b20a527f631b8ccb50d52fb107
mphpX <138234182+mphpX@users.noreply.github.com>
2025-04-11 17:23:20 +0900

    [Silver II] Title: DFS와 BFS, Time: 64 ms, Memory: 35432 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,36 +1,48 @@
 import sys
 from collections import deque
-input=sys.stdin.readline
-n,m,v=map(int,input().split())
-graph=[[] for _ in range(n+1)]
-for i in range(m):
-    x,y=map(int,input().split())
+input = sys.stdin.readline
+
+n, m, v = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+
+# 간선 입력
+for _ in range(m):
+    x, y = map(int, input().split())
     graph[x].append(y)
     graph[y].append(x)
-for i in range(1,n+1):
+
+# 번호가 작은 정점부터 방문하도록 정렬
+for i in range(1, n + 1):
     graph[i].sort()
-def bfs(n,start):
-    bfs_visit=[False for _ in range(n+1)]
-    q=deque([start])
-    bfs_visit[start]=True
-    ans=[]
-    while(q):
-        cur=q.popleft()
+
+# DFS 방문 여부
+dfs_visit = [False for _ in range(n + 1)]
+
+# DFS 함수
+def dfs(start):
+    print(start, end=' ')
+    dfs_visit[start] = True
+    for i in graph[start]:
+        if not dfs_visit[i]:
+            dfs(i)
+
+# BFS 함수
+def bfs(start):
+    bfs_visit = [False for _ in range(n + 1)]
+    q = deque([start])
+    bfs_visit[start] = True
+    ans = []
+    while q:
+        cur = q.popleft()
         ans.append(cur)
         for i in graph[cur]:
-            if(bfs_visit[i]==False):
+            if not bfs_visit[i]:
                 q.append(i)
-                bfs_visit[i]=True
+                bfs_visit[i] = True
     return ans
-dfs_visit=[False for _ in range(n+1)]
-def dfs(start):
-    print(start,end=' ')
-    dfs_visit[start]=True
-    for i in graph[start]:
-        if(dfs_visit[i]==False):
-            dfs(i)
+
+# 실행 및 출력
 dfs(v)
 print()
-bfs_ans=bfs(n,v)
-for i in bfs_ans:
-    print(i,end=' ')
+for i in bfs(v):
+    print(i, end=' ')
