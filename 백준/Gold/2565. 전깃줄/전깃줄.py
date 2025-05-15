819bf55fed945f1a2e489998fb43f348f81ffe3b
mphpX <138234182+mphpX@users.noreply.github.com>
2025-05-15 16:34:36 +0900

    [Gold V] Title: 전깃줄, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.py
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -1,13 +1,12 @@
 import sys
+input=sys.stdin.readline
 n=int(input())
-l=[]
-pdp=[1 for i in range(n)]
-for i in range(n):
-    a,b=map(int,sys.stdin.readline().split())
-    l.append([a,b])
-l.sort(key=lambda x: x[0])
-for i in range(n):
+graph=[list(map(int,input().split())) for _ in range(n)]
+dp=[1 for _ in range(n)]
+graph.sort()
+for i in range(1,n):
     for j in range(i):
-        if(l[i][1]>l[j][1]):
-            pdp[i]=max(pdp[j]+1,pdp[i])
-print(n-max(pdp))
+        if(graph[j][1]<graph[i][1]):
+            dp[i]=max(dp[j]+1,dp[i])
+
+print(n-max(dp))
