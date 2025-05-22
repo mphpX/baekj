c734547f18c8f34562ca32dc6e4abae456be87b6
mphpX <138234182+mphpX@users.noreply.github.com>
2025-05-22 22:54:51 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,12 @@
+chant = input().strip()
+
+if chant == "SONGDO":
+    print("HIGHSCHOOL")
+elif chant == "CODE":
+    print("MASTER")
+elif chant == "2023":
+    print("0611")
+elif chant == "ALGORITHM":
+    print("CONTEST")
+else:
+    print("UNKNOWN")
