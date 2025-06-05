5bc1ce68d7006598d86ef21ef682c3a6754e0cd1
mphpX <138234182+mphpX@users.noreply.github.com>
2025-06-05 15:12:33 +0900

    [Gold V] Title: 감소하는 수, Time: 60 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.py
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.py
@@ -2,14 +2,16 @@ from collections import deque
 n=int(input())
 basic=deque([1,2,3,4,5,6,7,8,9])
 ans=[0,1,2,3,4,5,6,7,8,9]
-start=10
+ct=9
 while(basic):
+    if(ct>=n):
+        break
     cur=basic.popleft()
     for i in range(cur%10):
         ans.append(cur*10+i)
         basic.append(cur*10+i)
-if(len(ans)>n):
+        ct+=1
+if(ct>= n):
     print(ans[n])
 else:
     print(-1)
-
