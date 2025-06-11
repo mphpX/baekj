e615ec67997130fdb57435467930ab5001865dd2
mphpX <138234182+mphpX@users.noreply.github.com>
2025-06-11 23:56:00 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 1652 ms, Memory: 208892 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -1,19 +1,15 @@
 n=int(input())
-sample=[['*','*','*'],['*',' ','*'],['*','*','*']]
 
 def recur2(m,x,y):
-    if(m==3):
-        print(sample[x%3][y%3],end='')
-        return
+    if(m==1):
+        print('*',end='')
     else:
         if((x%m)//(m//3)==1 and (y%m)//(m//3)==1):
             print(' ',end='')
             return
         recur2(m//3,x,y)
-if(n==1):
-    print('*')
-else:
-    for i in range(n):
-        for j in range(n):
-            recur2(n,i,j)
-        print()
+
+for i in range(n):
+    for j in range(n):
+        recur2(n,i,j)
+    print()
