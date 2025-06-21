b4311ad8fcababa86fabe0deb6511ab9ca9f835e
mphpX <138234182+mphpX@users.noreply.github.com>
2025-06-21 18:28:30 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
@@ -0,0 +1,13 @@
+while True:
+    data = input()
+    name, age, weight = data.split()
+    age = int(age)
+    weight = int(weight)
+
+    if name == '#' and age == 0 and weight == 0:
+        break
+
+    if age > 17 or weight >= 80:
+        print(f"{name} Senior")
+    else:
+        print(f"{name} Junior")
