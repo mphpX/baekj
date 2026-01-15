cab455e48a55792038dda915180d2c0c5fab2333
mphpX <138234182+mphpX@users.noreply.github.com>
2026-01-15 09:50:10 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.04 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -1,16 +1,12 @@
 class Solution {
     public String solution(String myString) {
-        int num = 0;
-        String answer = "";
-        for(int i = 0; i< myString.length() ; i++){
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < myString.length(); i++){
             char cur = myString.charAt(i);
-            num = cur;
-            if(96 < num && num < 123){
-                num -= 32;
-                cur = (char)num;
-            }
-            answer+=cur;
+            if('a' <= cur && cur <= 'z') cur = (char)(cur - 32);
+            sb.append(cur);
         }
-        return answer;
+        return sb.toString();
     }
 }
