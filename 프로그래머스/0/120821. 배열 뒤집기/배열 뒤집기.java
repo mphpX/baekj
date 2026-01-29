45e2ed6e9774d165b06418e16b5cf49aa6876eaf
mphpX <138234182+mphpX@users.noreply.github.com>
2026-01-29 17:40:57 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int [num_list.length];
+        int n = num_list.length - 1;
+        for(int i: num_list) answer[n--]= i;
+        return answer;
+    }
+}
