399ef6fe8792b3232fcd7c9b142196bc8edc48e8
mphpX <138234182+mphpX@users.noreply.github.com>
2026-02-08 16:22:47 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,001 ms, Memory: 91,392 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,21 +1,37 @@
-T = int(input())
-def back_track(n,menu,limit, s, cur_k, cur_p, ans):
-    if(limit<cur_k):
-        return
-    ans.add(cur_p)
-    for i in range(s, n):
-        next_k = cur_k+ menu[i][1]
-        next_p = cur_p+ menu[i][0]
-        if(next_k <= limit):
-            back_track(n,menu,limit, i+1, next_k, next_p, ans)
-    
-for test_case in range(1, T + 1):
-    n, l = map(int,input().split())
-    menu=[]
-    visited=[0 for _ in range(n)]
-    ans= set()
-    for i in range(n):
-        t,k= map(int,input().split())
-        menu.append((t,k))
-    back_track(n,menu,l, 0, 0, 0, ans)
-    print('#',test_case,' ',max(ans),sep='')
+import java.io.*;
+import java.util.*;
+public class Solution {
+
+    static int N, L, Best;
+    static int[] Scores, Cals;
+    static void dfs(int idx, int scoreSum, int calSum){
+        if(calSum > L) return;
+        if(idx == N){
+            if(scoreSum > Best) Best = scoreSum;
+            return;
+        }
+        dfs(idx+1, scoreSum + Scores[idx], calSum + Cals[idx]);
+        dfs(idx+1, scoreSum, calSum);
+    }
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int t;
+        t = Integer.parseInt(br.readLine());
+        for(int tc = 1; tc<=t; tc++){
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            L = Integer.parseInt(st.nextToken());
+            Scores= new int[N];
+            Cals = new int[N];
+            for(int i = 0; i < N; i++){
+                st= new StringTokenizer(br.readLine());
+                Scores[i]= Integer.parseInt(st.nextToken());
+                Cals[i]= Integer.parseInt(st.nextToken());
+            }
+            Best = 0;
+            dfs(0, 0, 0);
+            System.out.printf("#%d %d\n",tc, Best);
+        }
+    }
+}
