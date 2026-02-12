3b4c5757f2e869b5fb4d3cdf3cbdc04e8b6819a1
mphpX <138234182+mphpX@users.noreply.github.com>
2026-02-12 12:40:44 +0900

    [Bronze II] Title: 나머지, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,11 +1,9 @@
-import sys
-arr=[]
-for i in range(42):
-    arr.append(0)
+num_list=[]
 for i in range(10):
-    x=int(input())
-    arr[x%42]+=1
-ct=0
-for i in range(42):
-    if(arr[i]>0):ct+=1
-print(ct)
+    num_list.append(int(input()))
+# print(num_list)
+
+left_list=[]
+for j in num_list:
+    left_list.append(j % 42)
+print(len(list(set(left_list))))
