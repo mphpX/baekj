579f1ee190efd37e55089c5690a0797cc33f1e1e
mphpX <138234182+mphpX@users.noreply.github.com>
2026-02-26 13:32:19 +0900

    [Bronze V] Title: 문자열, Time: 96 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        for(int i= 0; i < n; i++){
+            String str = br.readLine();
+            bw.write(str.charAt(0));
+            bw.write(str.charAt(str.length()-1));
+            bw.write('\n');
+        }
+        bw.flush();
+    }
+}
