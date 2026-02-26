12eb7d932f1d7040b18f0fe7542ea6239d09ba96
mphpX <138234182+mphpX@users.noreply.github.com>
2026-02-26 17:15:00 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 116 ms, Memory: 14484 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] org = {1, 1, 2, 2, 2, 8};
+		for(int i = 0; i < 6; i++) {
+			System.out.printf("%d ",org[i]- Integer.parseInt(st.nextToken()));
+		}
+	}
+}
