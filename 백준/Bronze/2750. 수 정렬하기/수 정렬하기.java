3db9390629d12b8b9f37e4f70bfd604859bd821b
mphpX <138234182+mphpX@users.noreply.github.com>
2026-03-05 09:18:44 +0900

    [Bronze II] Title: 수 정렬하기, Time: 148 ms, Memory: 14892 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	static int[] arr;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		arr = new int[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		for(int i = 0; i < n; i++) {
+			for(int j = 1; j < n-i; j++) {
+				if(arr[j-1] > arr[j]) {
+					int tmp = arr[j-1];
+					arr[j-1] = arr[j];
+					arr[j] = tmp;
+				}
+			}
+		}
+		for(int i = 0; i < n; i++) {
+			System.out.println(arr[i]);
+		}
+		
+	}
+}
