d76b64fa184e3243f922082078b4e241fec9171f
mphpX <138234182+mphpX@users.noreply.github.com>
2026-03-17 22:59:53 +0900

    [Silver IV] Title: 스택 2, Time: 4616 ms, Memory: 254180 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static int gcd(int a, int b){
+        if(a== 0) return b;
+        else return gcd(b%a, a);
+    }
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        Stack<Integer> stack = new Stack<Integer>(); 
+        int n = Integer.parseInt(br.readLine());
+        int command;
+        int x;
+        for(int i = 0; i < n; i++){
+            st= new StringTokenizer(br.readLine());
+            command = Integer.parseInt(st.nextToken());
+            switch (command) {
+                case 1:
+                    x= Integer.parseInt(st.nextToken());
+                    stack.push(x);
+                    break;
+                case 2:
+                    if(stack.isEmpty()){
+                        System.out.println(-1);
+                    }else{
+                        System.out.println(stack.pop());
+                    }
+                    break;
+                case 3:
+                    System.out.println(stack.size());
+                    break;
+                case 4:
+                    System.out.println((stack.isEmpty()) ? 1 : 0);
+                    break;
+                case 5:
+                    System.out.println((stack.isEmpty()) ? -1 : stack.peek());                    
+                default:
+                    break;
+            }
+        }
+    }
+}
