067e42c85a13cae876cc9533c04714feab62f2b0
mphpX <138234182+mphpX@users.noreply.github.com>
2026-03-21 23:23:17 +0900

    [Silver IV] Title: 스택 2, Time: 4500 ms, Memory: 233024 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -1,10 +1,6 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    public static int gcd(int a, int b){
-        if(a== 0) return b;
-        else return gcd(b%a, a);
-    }
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
