657b72a38cf0dec24cedd644a40b691cbf01182d
mphpX <138234182+mphpX@users.noreply.github.com>
2026-04-02 21:38:54 +0900

    [level 2] Title: 스킬트리, Time: 0.04 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/README.md
+++ 프로그래머스/2/49993. 스킬트리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 스킬트리 - 49993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993) 
 
 ### 성능 요약
 
-메모리: 90.4 MB, 시간: 0.05 ms
+메모리: 86.7 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 02일 21:16:30
+2026년 04월 02일 21:38:52
 
 ### 문제 설명
 
--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -5,31 +5,20 @@ class Solution {
         int[] skill_idx = new int[skill.length()];
         for(int i = 0; i < skill.length(); i++){
             skill_idx[i]= (int)skill.charAt(i) - 'A';
-            alpha[skill_idx[i]] = -1;
+            alpha[skill_idx[i]] = i+1;
         }
-        alpha[skill_idx[0]]= 0;
         for(int i = 0; i < skill_trees.length; i++){
-            for(int j = 0; j < 26; j++){
-                alpha[j] = 0;
-            }
-            for(int j = 0; j < skill.length(); j++){
-                skill_idx[j]= (int)skill.charAt(j) - 'A';
-                alpha[skill_idx[j]] = -1;
-            }
-            alpha[skill_idx[0]]= 0;
             int isit= 1;
-            int cur = 0;
+            int cur = 1;
             for(int j = 0; j < skill_trees[i].length(); j++){
                 int cur_s = (int)skill_trees[i].charAt(j) - 'A';
-                if(alpha[cur_s]==-1){
-                    isit = 0;
-                    break;
-                }else{
-                    if(skill_idx[cur]==cur_s){
+                if(alpha[cur_s]==0)continue;
+                else{
+                    if(cur == alpha[cur_s]){
                         cur+=1;
-                        alpha[cur_s]= 1;
-                        if(cur== skill.length()) break;
-                        alpha[skill_idx[cur]]=0;
+                    }else{
+                        isit = 0;
+                        break;
                     }
                 }
             }
