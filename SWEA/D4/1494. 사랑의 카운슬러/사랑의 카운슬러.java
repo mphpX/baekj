c5c3907de60c7f2bb7032d1cd5adfecd889192d1
mphpX <138234182+mphpX@users.noreply.github.com>
2026-04-15 15:53:42 +0900

    [D4] Title: 사랑의 카운슬러, Time: 300 ms, Memory: 28,060 KB -BaekjoonHub

--- SWEA/D4/1494. 사랑의 카운슬러/사랑의 카운슬러.java
+++ SWEA/D4/1494. 사랑의 카운슬러/사랑의 카운슬러.java
@@ -1,64 +1,60 @@
-import java.io.*;
+
 import java.util.*;
-
-public class Solution {
-    static int N;
-    static int[] x, y;
-    static long ans;
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        int T = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= T; tc++) {
-
-            N = Integer.parseInt(br.readLine());
-
-            x = new int[N];
-            y = new int[N];
-
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                x[i] = Integer.parseInt(st.nextToken());
-                y[i] = Integer.parseInt(st.nextToken());
-            }
-
-            ans = Long.MAX_VALUE;
-
-            bt(0, 0, 0, 0, 0, 0);
-
-            System.out.println("#" + tc + " " + ans);
-        }
-    }
-
-    static void bt(int idx, int cntA,
-                   long sumAX, long sumAY,
-                   long sumBX, long sumBY) {
-
-        // 가지치기
-        if (cntA > N / 2) return;
-        if (cntA + (N - idx) < N / 2) return;
-
-        if (idx == N) {
-            if (cntA == N / 2) {
-                long vx = sumAX - sumBX;
-                long vy = sumAY - sumBY;
-
-                long dist = vx * vx + vy * vy;
-                ans = Math.min(ans, dist);
-            }
-            return;
-        }
-
-        // A 그룹
-        bt(idx + 1, cntA + 1,
-           sumAX + x[idx], sumAY + y[idx],
-           sumBX, sumBY);
-
-        // B 그룹
-        bt(idx + 1, cntA,
-           sumAX, sumAY,
-           sumBX + x[idx], sumBY + y[idx]);
-    }
+import java.io.*;
+public class Solution {	
+	static int[] visited;
+	static int m;
+	static int n;
+	static long ans;
+	static class Node{
+		long x;
+		long y;
+		public Node(long x, long y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	static ArrayList<Node> list;
+	static void backtrack(int ct, int idx, long cur_x, long cur_y) {
+		if(ct == n /2) {
+			for(int i = 0; i < n; i++) {
+				if(visited[i] == 0) {
+					cur_x -= list.get(i).x;
+					cur_y -= list.get(i).y;
+				}
+			}
+			long tmp = cur_x*cur_x + cur_y*cur_y;
+			ans= Math.min(ans, tmp);
+			return;
+		}
+		for(int i = idx+1; i < n; i++){
+			if(visited[i] == 0) {
+				visited[i] = 1;
+				backtrack( ct+1, i, cur_x + list.get(i).x, cur_y + list.get(i).y);
+				visited[i] = 0;
+			}
+		}	
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int test= Integer.parseInt(br.readLine());
+		//테스트 케이스
+		for(int tc= 1; tc <= test; tc++) {
+			n = Integer.parseInt(br.readLine());
+			visited = new int[n];
+			list = new ArrayList<>();
+			ans = Long.MAX_VALUE;
+			for(int i = 0; i < n; i++){
+				st= new StringTokenizer(br.readLine());
+				int x=  Integer.parseInt(st.nextToken());
+				int y =  Integer.parseInt(st.nextToken());
+				list.add(new Node(x,y));
+			}
+			backtrack(0, -1, 0, 0);
+			bw.write("#"+tc+" "+ans+"\n");
+		}
+		bw.flush();
+	}
 }
