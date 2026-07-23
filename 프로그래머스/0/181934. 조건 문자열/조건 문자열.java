73507b1eec1149a7c352ed33e4a84a90172a59f2
mphpX <138234182+mphpX@users.noreply.github.com>
2026-07-23 12:37:06 +0900

    [level 0] Title: 조건 문자열, Time: 0.03 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        int answer = 0;
+        if(n < m){
+            if(ineq.equals("<")){
+                answer = 1;
+            }
+        }else if( n > m){
+            if(ineq.equals(">")){
+                answer = 1;
+            }
+        }else{
+            if(eq.equals("=")){
+                answer = 1;
+            }
+        }
+            
+        return answer;
+    }
+}
