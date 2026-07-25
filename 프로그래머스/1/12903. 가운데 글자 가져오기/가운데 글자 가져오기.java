fa89e72d6fd0927a2ca6b87bfe6497f0bf5ec76f
mphpX <138234182+mphpX@users.noreply.github.com>
2026-07-25 15:31:59 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.70 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int n = s.length();
+        if(n % 2 == 1){
+            answer+=s.substring(n/2, n/2+1);
+        }else{
+            answer+=s.substring(n/2-1, n/2+1);
+        }
+        
+        return answer;
+    }
+}
