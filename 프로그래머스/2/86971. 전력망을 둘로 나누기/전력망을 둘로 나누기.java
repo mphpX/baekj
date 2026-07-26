d53ca39665ee29f8f455f6ea3503725abbb571ce
mphpX <138234182+mphpX@users.noreply.github.com>
2026-07-26 13:26:56 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.40 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,52 +1,57 @@
 import java.util.*;
-
 class Solution {
-    int N;
-    int ans;
+    int[][] graph;
+    int mn= 101;
     boolean[] visited;
-    List<Integer>[] graph;
-
-    public int solution(int n, int[][] wires) {
-        N = n;
-        ans = Integer.MAX_VALUE;
-
-        graph = new ArrayList[N+1];
-        for (int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
-        for (int[] w : wires) {
-            graph[w[0]].add(w[1]);
-            graph[w[1]].add(w[0]);
-        }
-
-        for (int[] w : wires) {
-            int u = w[0], v = w[1];
-
-            graph[u].remove(Integer.valueOf(v));
-            graph[v].remove(Integer.valueOf(u));
-
-            visited = new boolean[N+1];
-            int cnt = dfs(1);
-
-            ans = Math.min(ans, Math.abs(N - 2 * cnt));
-
-            graph[u].add(v);
-            graph[v].add(u);
-        }
-
-        return ans;
-    }
-
-    int dfs(int node) {
-        visited[node] = true;
-        int cnt = 1;
-
-        for (int next : graph[node]) {
-            if (!visited[next]) {
-                cnt += dfs(next);
+    public int bfs(int x, int n){
+        Deque<Integer> dq= new ArrayDeque<>();
+        dq.add(x);
+        visited[x]= true;
+        int ct = 1;
+        while(!dq.isEmpty()){
+            int cur = dq.poll();
+            for(int i = 1; i <= n; i++){
+                if(graph[cur][i]== 1 && !visited[i]){
+                    visited[i]= true;
+                    dq.add(i);
+                    ct+=1;
+                }
             }
         }
-        return cnt;
+        return ct;
+    }
+    public void back_track(int n, int[][] wires){
+        for(int i = 0; i < wires.length; i++){
+            for(int j = 0; j < n+1; j++){
+                visited[j]= false;
+            }
+            int idx = 0;
+            int[] diff= new int[2];
+            int x = wires[i][0];
+            int y = wires[i][1];
+            graph[x][y] = 0;
+            graph[y][x] = 0;
+            for(int j = 1; j < n+1; j++){
+                if(!visited[j]){
+                    diff[idx++]= bfs(j, n);
+                }
+            }
+            mn = Math.min(Math.abs(diff[1]- diff[0]), mn);
+            graph[x][y] = 1;
+            graph[y][x] = 1;
+        }
+    }
+    public int solution(int n, int[][] wires) {
+        int answer = -1;
+        graph= new int[n+1][n+1];
+        visited= new boolean[n+1];
+        for(int i = 0; i < wires.length; i++){
+            int x= wires[i][0];
+            int y= wires[i][1];
+            graph[x][y]= 1;
+            graph[y][x]= 1;
+        }
+        back_track(n, wires);
+        return mn;
     }
 }
